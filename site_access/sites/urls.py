eb472c7f92a2391e5e3993f74158e437bfc6c3c5
Stephen-Agbeyebiawo <stephen.agbeyebiawo@gmail.com>
2023-04-24 20:19:00 +0100

    Login logic and Dashboard completed

--- site_access/sites/urls.py
+++ site_access/sites/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("dashboard", views.dashboard, name="dashboard")
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("logout", views.logout_func, name="logout")
 ]
--- site_access/sites/views.py
+++ site_access/sites/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    return render(request, "sites/login.html", {
+        "message":"Ener Username and Password"
+    })
+
 def login_view(request):
-    return render(request, "sites/login.html")
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("dashboard"))
+    else:
+        return render(request, "sites/login.html", {
+            "message":"Invalid Credentials"
+        })
+
+def logout_func(request):
+    logout(request)
+    return render(request, "sites/login.html", {
+        "message":"Successfully Logged Out"
+    })
 
 def dashboard(request):
     return render(request, "dashboard/dashboard.html")
