4b2b1665eac9fc0a168b8825c4a5d6164ce2f2d7
Stephen-Agbeyebiawo <stephen.agbeyebiawo@gmail.com>
2023-04-24 18:15:44 +0100

    added dashboard

--- site_access/sites/urls.py
+++ site_access/sites/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("login", views.login_view, name="login")
+    path("login", views.login_view, name="login"),
+    path("dashboard", views.dashboard, name="dashboard")
 ]
--- site_access/sites/views.py
+++ site_access/sites/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_view(request):
     return render(request, "sites/login.html")
+
+def dashboard(request):
+    return render(request, "dashboard/dashboard.html")
